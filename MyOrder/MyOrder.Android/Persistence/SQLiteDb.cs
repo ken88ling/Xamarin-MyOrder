a556847fe0966542454d95bb17d82fa250ed620c
Ken Ling <ken88ling@gmail.com>
2017-05-15 00:52:08 +0800

    inject SQLite

---
+++ MyOrder/MyOrder.Android/Persistence/SQLiteDb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using MyOrder.Persistence;
+
+[assembly:Dependency(typeof(ISqLiteDb))]
+
+namespace MyOrder.Droid.Persistence
+{
+    public class SQLiteDb : ISqLiteDb
+    {
+        public SQLiteAsyncConnection GetConnection()
+        {
+            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var path = Path.Combine(documentsPath, "MySQLite.db3");
+
+            return new SQLiteAsyncConnection(path);
+        }
+    }
+}
